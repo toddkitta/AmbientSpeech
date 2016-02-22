c50ae8751fb9e50a64404764d404936caf797e6f
toddkitta <toddkitta@outlook.com>
2016-02-21 20:39:04 -0600

    Initial commit.

---
+++ AmbientSpeech/AmbientSpeechForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace AmbientSpeech
+{
+    partial class AmbientSpeechForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPartial = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPartial
+            // 
+            this.txtPartial.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPartial.Location = new System.Drawing.Point(12, 12);
+            this.txtPartial.Multiline = true;
+            this.txtPartial.Name = "txtPartial";
+            this.txtPartial.ReadOnly = true;
+            this.txtPartial.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPartial.Size = new System.Drawing.Size(915, 378);
+            this.txtPartial.TabIndex = 0;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFinal.Location = new System.Drawing.Point(12, 396);
+            this.txtFinal.Multiline = true;
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.ReadOnly = true;
+            this.txtFinal.Size = new System.Drawing.Size(915, 76);
+            this.txtFinal.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 478);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(915, 110);
+            this.textBox1.TabIndex = 2;
+            // 
+            // AmbientSpeechForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 600);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtPartial);
+            this.MinimumSize = new System.Drawing.Size(957, 647);
+            this.Name = "AmbientSpeechForm";
+            this.Text = "Ambient Speech";
+            this.Load += new System.EventHandler(this.AmbientSpeechForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPartial;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ AmbientSpeech/AmbientSpeechForm.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.ProjectOxford.SpeechRecognition;
+using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace AmbientSpeech
+{
+    public partial class AmbientSpeechForm : Form
+    {
+        private delegate void WriteTextBoxLineCallback(TextBox textBox, string text);
+        private delegate void ClearTextBoxCallback(TextBox textBox);
+
+        private MicrophoneRecognitionClient micClient;
+        private string oxfordKey;
+
+        private const string LANGUAGE = "en-us";
+
+        public AmbientSpeechForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AmbientSpeechForm_Load(object sender, EventArgs e)
+        {
+            oxfordKey = ConfigurationManager.AppSettings["OxfordKey"];
+            InitMicClient();
+        }
+
+        private void InitMicClient()
+        {
+            if(micClient != null)
+            {
+                micClient.Dispose();
+                micClient = null;
+            }
+
+            micClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
+                SpeechRecognitionMode.LongDictation,
+                LANGUAGE,
+                oxfordKey);
+
+            micClient.OnMicrophoneStatus += OnMicrophoneStatus;
+            micClient.OnPartialResponseReceived += OnPartialResponseReceivedHandler;
+            micClient.OnResponseReceived += OnMicDictationResponseReceivedHandler;
+            micClient.OnConversationError += OnConversationErrorHandler;
+
+            micClient.StartMicAndRecognition();
+        }
+
+        private void WriteTextBoxLine(TextBox txt, string message)
+        {
+            if (txt.InvokeRequired)
+            {
+                WriteTextBoxLineCallback d = new WriteTextBoxLineCallback(WriteTextBoxLine);
+                Invoke(d, new object[] { txt, message });
+            }
+            else
+            {
+                if (!txt.IsDisposed)
+                {
+                    txt.AppendText(message);
+                    txt.AppendText(Environment.NewLine);
+                }
+            }
+        }
+
+        private void ClearTextBox(TextBox txt)
+        {
+            if (txt.InvokeRequired)
+            {
+                ClearTextBoxCallback d = new ClearTextBoxCallback(ClearTextBox);
+                Invoke(d, new object[] { txt });
+            }
+            else
+            {
+                txt.Clear();
+            }
+        }
+
+        #region Event Handlers
+
+        private void OnConversationErrorHandler(object sender, SpeechErrorEventArgs e)
+        {
+            ClearTextBox(txtFinal);
+
+            WriteTextBoxLine(txtFinal, String.Format("ERROR CODE: {0}", e.SpeechErrorCode.ToString()));
+            WriteTextBoxLine(txtFinal, String.Format("ERROR TEXT: {0}", e.SpeechErrorText));
+            WriteTextBoxLine(txtFinal, String.Empty);
+
+            InitMicClient();
+        }
+
+        private void OnMicDictationResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
+        {
+            ClearTextBox(txtFinal);
+
+            if (e.PhraseResponse.RecognitionStatus == RecognitionStatus.EndOfDictation ||
+                e.PhraseResponse.RecognitionStatus == RecognitionStatus.DictationEndSilenceTimeout)
+            {
+                micClient.EndMicAndRecognition();
+            }
+
+            if (e.PhraseResponse.Results.Length == 0)
+            {
+                WriteTextBoxLine(txtFinal, "NOTHING RETURNED");
+            }
+            else
+            {
+                for (int i = 0; i < e.PhraseResponse.Results.Length; i++)
+                {
+                    WriteTextBoxLine(txtFinal,
+                        String.Format("[{0}]: \"{2}\" (Confidence = {1})",
+                            i,
+                            e.PhraseResponse.Results[i].Confidence,
+                            e.PhraseResponse.Results[i].DisplayText));
+                }
+                WriteTextBoxLine(txtFinal, String.Empty);
+            }
+
+            InitMicClient();
+        }
+
+        private void OnPartialResponseReceivedHandler(object sender, PartialSpeechResponseEventArgs e)
+        {
+            WriteTextBoxLine(txtPartial, e.PartialResult);
+        }
+
+        private void OnMicrophoneStatus(object sender, MicrophoneEventArgs e)
+        {
+            WriteTextBoxLine(txtPartial, e.Recording ? "LISTENING..." : "NOT LISTENING (for some reason...)");
+            WriteTextBoxLine(txtPartial, String.Empty);
+        }
+
+        #endregion
+    }
+}
